e33316be590a71556e57ff753c9526f96378e4b7
Aileen Rae <aileenlrae@gmail.com>
2021-07-26 17:48:34 +0100

    Start mobile nav open transition

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+const body = document.querySelector("body");
 const menuOpen = document.getElementById("menu-open");
 const menuClose = document.getElementById("menu-close");
 const menu = document.getElementById("mobile-menu");
 const firstMenuItem = document.getElementById("first-menu-item");
 
 const openMenu = () => {
+  body.classList.add("disable-scroll");
   menu.classList.add("active");
   firstMenuItem.focus();
 };
 
 const closeMenu = () => {
+  body.classList.remove("disable-scroll");
   menu.classList.remove("active");
   menuOpen.focus();
 };
