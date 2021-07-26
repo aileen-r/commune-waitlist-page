1e80a7f2b12f6c304eb531ead8c0ce8765d53fe0
Aileen Rae <aileenlrae@gmail.com>
2021-07-26 16:30:48 +0100

    Mobile hamburger toggle

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world! 👋");
+const menuOpen = document.getElementById("menu-open");
+const menuClose = document.getElementById("menu-close");
+const menu = document.getElementById("mobile-menu");
+const firstMenuItem = document.getElementById("first-menu-item");
+
+const openMenu = () => {
+  menu.classList.add("active");
+  firstMenuItem.focus();
+};
+
+const closeMenu = () => {
+  menu.classList.remove("active");
+  menuOpen.focus();
+};
+
+menuOpen.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
